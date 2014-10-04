91251b7541d3263c1e42dea79fd99f55976ad11d
andresperezpayeras <andresp@ati.es>
2014-10-04 20:38:21 +0200

    indent

--- app-skel.js
+++ app-skel.js
@@ -1,19 +1,15 @@
-var work=function(){
-  console.log("hciendo algo");
+var work = function() {
+	console.log("hciendo algo");
 };
 
-var dowork = function(f){
-  console.log("start work");
-  try {
-  f();
-}
-catch(ex){
-console.log(ex);
-}
-console.log("end work");
+var dowork = function(f) {
+	console.log("start work");
+	try {
+		f();
+	} catch (ex) {
+		console.log(ex);
+	}
+	console.log("end work");
 };
 
 dowork(work);
-
-
-
